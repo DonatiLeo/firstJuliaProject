620551b560f287c1bc2fe1ed192443337b9ee089
Leo Donati <leo.donati@unice.fr>
2023-03-09 10:48:17 +0100

    test de copilot in julia

--- exo1.jl
+++ exo1.jl
@@ -11,9 +11,36 @@ function max(x)
     return m
 end
 
+
+
 #test this function
 x = [1, 2, 3, 4, 5]
 max(x)
+
+# calcule le factoriel de n récursivement
+function fact(n)
+    if n == 0
+        return 1
+    else
+        return n * fact(n-1)
+    end
+end
+
+# calcule le pgcd de deux entiers
+function pgcd(a, b)
+    if a == 0
+        return b
+    elseif b == 0
+        return a
+    elseif a == b
+        return a
+    elseif a > b
+        return pgcd(a-b, b)
+    else
+        return pgcd(a, b-a)
+    end
+end
+
 
 
 
